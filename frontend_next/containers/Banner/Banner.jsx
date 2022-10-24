f3b43796e96e8f8a2ea5b086f22ae311ef5e5ea0
Jordi <im.jordiparadelo@gmail.com>
2022-10-24 16:16:50 +0200

    updates/banner-component

--- frontend_next/containers/Banner/Banner.jsx
+++ frontend_next/containers/Banner/Banner.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 // styles
-import styles from 'styles/Banner.module.scss'
+import styles from "styles/Banner.module.scss";
 
-const Banner = () => {
-    return (
-        <section className={styles.Banner}>
-            Banner
-        </section>
-    );
+const Banner = ({ title, description, cta, background }) => {
+  return (
+    <section className={`${styles.Banner}`}>
+      <div className="wrapper">
+        <h2 className={`${styles.Banner__title}`}>
+          Las mejores condiciones del mercado inmobiliario
+        </h2>
+        <p className={`${styles.Banner__description}`}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus non
+          fuga atque animi magni commodi, omnis reprehenderit assumenda facere
+          tempore architecto ea! Modi placeat aliquid assumenda nostrum! Nulla,
+          quia earum.
+        </p>
+        <button className={`${styles.Banner__cta}`}>
+          Descubre nuestas tarifas
+        </button>
+      </div>
+      <div className={`${styles.Banner__background}`}></div>
+    </section>
+  );
 };
 
 export default Banner;
